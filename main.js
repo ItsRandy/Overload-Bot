62632bb36de9de8ce8c8936d3909f1d930566cb1
Randy <72589112+ItsRandy@users.noreply.github.com>
2020-11-08 17:35:31 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+const Discord = require('discord.js');
+const { Client, RichEmbed, } = require ("discord.js");
+const client = new Discord.Client();
+const prefix = "_";
+
+
+const { GiveawaysManager } = require('discord-giveaways');
+
+client.giveawaysManager = new GiveawaysManager(client, {
+    storage: "./giveaways.json",
+    updateCountdownEvery: 5000,
+    default: {
+        botsCanWin: false,
+        exemptPermissions: ["MANAGE_MESSAGES", "ADMINISTRATOR"],
+        embedColor: "#FF0000",
+        reaction: "🎉"
+    }
+});
+
+const fs = require('fs');
+ 
+client.commands = new Discord.Collection();
+ 
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+ 
+    client.commands.set(command.name, command);
+}
+ 
+ 
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag} And we are in ${client.guilds.cache.size} Servers `);
+
+})
+
+
+ 
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+ 
+    if(command === 'ping'){
+        client.prefix = prefix
+
+        client.commands.get('ping').execute(message, args);
+    } 
+
+    
+})
+
+client.on("message", async message => {
+
+    if(message.author.bot) return;
+    if(message.channel.type === 'dm') return;
+
+    if(message.content.startsWith(prefix)) {
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+
+        const command = args.shift().toLowerCase();
+
+        if(!client.commands.has(command)) return;
+
+
+        try {
+            client.commands.get(command).run(client, message, args);
+
+        } catch (error){
+            console.error(error);
+        }
+    }
+})
+
+
+
+
+
+client.login('place token here');
